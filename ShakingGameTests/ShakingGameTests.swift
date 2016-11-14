88ad11a1ea2093566e4eaa5df11553c0055fb1df
yasamins <yasamin.salami@yahoo.com>
2016-11-14 20:47:37 +0200

    Initial Commit

---
+++ ShakingGameTests/ShakingGameTests.swift
@@ -0,0 +1,36 @@
+//
+//  ShakingGameTests.swift
+//  ShakingGameTests
+//
+//  Created by yasamin  on 14.11.2016.
+//  Copyright © 2016 yasamin. All rights reserved.
+//
+
+import XCTest
+@testable import ShakingGame
+
+class ShakingGameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
